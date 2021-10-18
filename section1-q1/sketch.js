38fa74a02a0810802e51e83e0366f278b074b266
MatsumotoMizuki <mmizuki0717@gmail.com>
2021-10-18 13:11:33 +0900

    Change section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -7,8 +7,9 @@ function setup(){
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  stroke(0,128,0)
+  arc(100 + 25, 100, 100, 100, QUARTER_PI*3, QUARTER_PI * 3 + PI);
+arc(100-25,100,100,100, QUARTER_PI*5, QUARTER_PI * 5 + PI)
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);

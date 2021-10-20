d7e36d34fd124bc274d4f56cc1939ec54c3ad0c2
MatsumotoMizuki <mmizuki0717@gmail.com>
2021-10-20 11:22:02 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+  for(let i=0;i<5;i++){
+stroke(0,0,255)
+let x=(i+1)*10
+noFill()
+  ellipse(50,50,x);
+}
+for(let i=5;i<10;i++){
+  stroke(255,0,0)
+  let x=(i+1)*10
+  noFill()
+  ellipse(50,50,x)
+}
 }

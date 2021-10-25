4aaab679e8dc387d8902a8a74dfd75b7b9e93bfe
MatsumotoMizuki <mmizuki0717@gmail.com>
2021-10-25 12:44:19 +0900

    section2-q2 Change

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,10 +2,37 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  noStroke();
+stroke(77)
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      if((i+j)%2==1){
+
+        fill(110,110,110);
+      }else {
+      noFill();
+        noStroke()
+
+}
+  rect(size*i,size*j,size,size);
+
+
+  if(j<3){
+    fill(255,0,0);
   }
+else if(j<5){
+  noFill();
+}
+else {
+  fill(0,0,0);
+
+}
+if((i+j)%2==0)
+{noFill()
+}
+circle(size*(i+1/2),size*(j+1/2),size*4/5)
+
+
+}
+}
 }

f8fc83fab7476c8b9a8d0b7648a3d3b47dcbaacb
MatsumotoMizuki <mmizuki0717@gmail.com>
2021-11-09 11:43:16 +0900

    Change section4-2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -8,8 +8,9 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   balls = [];
 }
-
+count=0;
 function draw(){
+  count++;
   background(160, 192, 255);
   for(let i = 0; i < balls.length; i++){
     let b = balls[i];
@@ -17,13 +18,18 @@ function draw(){
     b.x += b.vx;
     b.y += b.vy;
   }
+if(count % 30 == 0){
+const b={x:width/2,y:height/2,size:20,vx:random(-1,1),vy:random(-1,1)};
+balls.push(b);
+
+  }
 }
 
 function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(20,30), vx: dx, vy: dy };
     balls.push(b);
   }
 }

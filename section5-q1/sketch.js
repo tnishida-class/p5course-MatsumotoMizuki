f3784b934bb36be911dd6efd75bd27066c6252af
MatsumotoMizuki <mmizuki0717@gmail.com>
2021-11-16 01:46:33 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I want to tanni.",2,100,150,100,100);
 }
 
-function balloon(t){
+function balloon(t,a,b,c,px,py,){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
+  fill(a,b,c);
+  stroke(a,b,c);
+  rect(px, py, w + p * 2, h + p * 2);
+  triangle(px+w+p*2,py+h+p*2,px+w-p*2,py+h+p*2,px+w,py+h+p*6)
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  text(t, p+px, h + p+py);
 }
